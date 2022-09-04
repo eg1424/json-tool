73120015e1abb704286edfa29dee1b56d5293440
eg1424@gmail.com <eg1424@gmail.com>
2022-09-04 11:44:31 +0800

    add state handling

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,20 @@
-import {Form, Container ,Row, Col} from 'react-bootstrap';
+import React, {ChangeEvent, useState} from 'react';
+import {Form, Container ,Row, Col, Button} from 'react-bootstrap';
+
 
 const MainPage = ()=>{
+
+    interface JsonFormState {
+        input:string,
+        output?:string
+    }
+
+    const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
+        setJsonForm({input:e.target.value});
+    }
+
+    const [jsonForm,setJsonForm] = useState<JsonFormState>({input:"",output:""});
+
     return (
         <Form>
         <Container>
@@ -8,15 +22,18 @@ const MainPage = ()=>{
             <Col> 
             <Form.Group className="mb-3" controlId="jsonForm.input">
             <Form.Label>Input</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control as="textarea" rows={10} value={jsonForm.input} onChange={handleChange}/>
             </Form.Group>
             </Col>
             <Col>
             <Form.Group className="mb-3" controlId="jsonForm.output">
             <Form.Label>Output</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control as="textarea" rows={10} value={jsonForm.output} readOnly />
             </Form.Group>
             </Col>
+        </Row>
+        <Row>
+            <Button>Format</Button>
         </Row>
         </Container>
       </Form>
