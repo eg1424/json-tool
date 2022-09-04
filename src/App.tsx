2ab89d9fd907252236f5cf73aedeb19bb7104169
eg1424@gmail.com <eg1424@gmail.com>
2022-09-04 17:14:00 +0800

    display error message and add compre

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
     <div>
       <header>
       </header>
-      <body>
+      <div>
         <MainPage/>
-      </body>
+      </div>
     </div>
   );
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,18 +2,46 @@ import React, {ChangeEvent, useState} from 'react';
 import {Form, Container ,Row, Col, Button} from 'react-bootstrap';
 
 
+const ReactJsonViewCompare = require('react-json-view-compare');
+
 const MainPage = ()=>{
 
     interface JsonFormState {
-        input:string,
-        output?:string
+        inputLeft:string,
+        inputRight:string,
+        outputLeft?:Object,
+        outputRight?:Object,
+        errorLeft?:string,
+        errorRight?:string
     }
 
-    const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
-        setJsonForm({input:e.target.value});
+    const validJson = false;
+    
+    const [jsonForm,setJsonForm] = useState<JsonFormState>({inputLeft:"",inputRight:""});
+
+
+    const handleChangeLeft = (e:ChangeEvent<HTMLInputElement>)=>{
+        const { output: outputLeft, errorMessage: errorLeft } = parseJson(e);    
+
+        setJsonForm({...jsonForm ,inputLeft:e.target.value, outputLeft, errorLeft});
     }
 
-    const [jsonForm,setJsonForm] = useState<JsonFormState>({input:"",output:""});
+    const handleChangeRight = (e:ChangeEvent<HTMLInputElement>)=>{
+        const { output: outputRight, errorMessage: errorRight } = parseJson(e);  
+        setJsonForm({...jsonForm , inputRight:e.target.value, outputRight, errorRight});
+    }
+
+    const parseJson = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let output = {};
+        let errorMessage = "";
+        try {
+            output = JSON.parse(e.target.value);
+        } catch (error: any) {
+            errorMessage = error.message;
+        }
+        return { output, errorMessage };
+    }
+
 
     return (
         <Form>
@@ -21,24 +49,47 @@ const MainPage = ()=>{
         <Row>       
             <Col> 
             <Form.Group className="mb-3" controlId="jsonForm.input">
-            <Form.Label>Input</Form.Label>
-            <Form.Control as="textarea" rows={10} value={jsonForm.input} onChange={handleChange}/>
+            <Form.Label>Input 1</Form.Label>
+            <Form.Control as="textarea" rows={10} value={jsonForm.inputLeft} onChange={handleChangeLeft}/>
             </Form.Group>
             </Col>
             <Col>
             <Form.Group className="mb-3" controlId="jsonForm.output">
-            <Form.Label>Output</Form.Label>
-            <Form.Control as="textarea" rows={10} value={jsonForm.output} readOnly />
+            <Form.Label>Input 2</Form.Label>
+            <Form.Control as="textarea" rows={10} value={jsonForm.inputRight}  onChange={handleChangeRight}/>
             </Form.Group>
             </Col>
         </Row>
+        {(jsonForm.errorLeft || jsonForm.errorRight) &&
+
         <Row>
-            <Button>Format</Button>
+            <Col>
+            <Form.Group className="mb-3" controlId="jsonForm.errorLeft">
+            <Form.Label>Error 1</Form.Label>
+            <Form.Control as="textarea" rows={5} value={jsonForm.errorLeft}  readOnly className='text-danger'/>
+            </Form.Group>
+            </Col>
+            <Col>
+            <Form.Group className="mb-3" controlId="jsonForm.errorRight">
+            <Form.Label>Error 2</Form.Label>
+            <Form.Control as="textarea" rows={5} value={jsonForm.errorRight}  readOnly className='text-danger'/>
+            </Form.Group>
+            </Col>
         </Row>
+        }
+        {jsonForm.outputLeft && jsonForm.outputRight &&
+        <Row>
+            <Col>
+                <ReactJsonViewCompare oldData={ jsonForm.outputLeft} newData={jsonForm.outputRight} />
+            </Col>
+        </Row>
+        }
         </Container>
       </Form>
     )
 }
 
 export default MainPage;
+
+
 
